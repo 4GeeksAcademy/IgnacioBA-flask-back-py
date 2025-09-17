93ec1cdcd3b9497163a3e36fa929f60010197dc4
IgnacioBA9 <150173378+IgnacioBA9@users.noreply.github.com>
2025-09-17 17:30:35 +0000

    [feat:]New login and signup endpoints

--- src/app.py
+++ src/app.py
@@ -9,14 +9,18 @@ from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User
-#from models import Person
+from sqlalchemy import select
+from sqlalchemy.exc import SQLAlchemyError
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
+# from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace(
+        "postgres://", "postgresql://")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -26,15 +30,24 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "example_password"
+jwt = JWTManager(app)
+
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
@@ -44,6 +57,62 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    try:
+        data = request.get_json()
+        
+        email = data.get('email')
+        password = str(data.get('password'))
+        
+
+        if not email or not password:
+            return jsonify({"msg": "Email and password are required"}), 400
+
+        if User.query.filter_by(email=email).first():
+            return jsonify({"msg": "User already exists"}), 400
+
+        new_user = User(
+            email=email,
+            password=password
+           
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return jsonify({"msg": "User created successfully"}), 201
+
+    except Exception as e:
+        return jsonify({"error": "Server error", "details": str(e)}), 500
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        data = request.get_json()
+        email = data.get('email')
+        password = str(data.get('password'))
+
+        if not email or not password:
+            return jsonify({"msg": "Email and password are required"}), 400
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            return jsonify({"msg": "User not found"}), 404
+
+        if user.password != password:
+            return jsonify({"msg": "Incorrect password"}), 401
+
+        access_token = create_access_token(identity=str(user.id))
+        return jsonify(access_token=access_token), 200
+
+    except Exception as e:
+        return jsonify({"error": "Server error", "details": str(e)}), 500
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
